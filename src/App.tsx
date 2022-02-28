0919918f064f021addfe791760e0467f11994efb
S Harish <sreeramappaharish@gmail.com>
2022-02-28 19:13:44 +0530

    Add initial component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { TextField } from "@mui/material";
+import React, { useState } from "react";
+import PhoneInput from "react-phone-input-material-ui";
+import "react-phone-input-material-ui/lib/style.css";
+
+import "./App.css";
 
 function App() {
+  const [phone, setPhone] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{paddingTop: 30, width: 300, paddingLeft: 30}}>
+      <PhoneInput
+        // @ts-ignore
+        component={TextField}
+        inputProps={{variant: 'filled'}}
+        value={phone}
+        onChange={(phone: string) => setPhone(phone)}
+      />
     </div>
   );
 }
